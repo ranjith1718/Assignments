f706b528897be4ddc7d02edbeebcf23fa40d17e1
Ranjith.kumar <ranjeeth1718@gmail.com>
2025-02-22 18:06:13 +0530

    Assignment on Data types entry

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -3,7 +3,47 @@ package com.assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		float celsius = 25.5f;
+		boolean order = true;
+		double acc = 1000.50;
+		long phoneNumber = 1234567890l;
+		String email = "john.doe@example.com";
+		double location = 37.7749;
+		boolean status = true;
+		String occupation = "software Engineer";
+		String colour = "Blue";
+		int year = 2023;
+		int media = 1000000;
+		float movie = 7.5f;
+		char blood = 'A';
+		String book = "To Kill a Mockingbird";
+		int companyEmployee = 500;
+		String event = "2:30 PM";
+		String country = "united states";
+		String eye = "brown";
+		String birth = "new york city";
+		double twoCities = 200.5;
+
+		System.out.println("Temperature of a city in degrees Celsius:" + celsius);
+		System.out.println("Whether a customer has placed an order:" + order);
+		System.out.println("Person's phone number:" + phoneNumber);
+		System.out.println("Amount of money in a customer's bank account:" + acc);
+		System.out.println("Person's email address:" + email);
+		System.out.println("Coordinates of a location (latitude, longitude):" + location);
+		System.out.println("Person's marital status:" + status);
+		System.out.println("Person's occupation:" + occupation);
+		System.out.println("Person's favourite colour:" + colour);
+		System.out.println("Current year:" + year);
+		System.out.println("Number of followers on a social media platform:" + media);
+		System.out.println("Rating of a movie:" + movie);
+		System.out.println("Person's blood type:" + blood);
+		System.out.println("Title of a book:" + book);
+		System.out.println("Number of employees in a company:" + companyEmployee);
+		System.out.println("Time of an event:" + event);
+		System.out.println("Name of a country:" + country);
+		System.out.println("Person's eye color:" + eye);
+		System.out.println("Person's birthplace:" + birth);
+		System.out.println("istance between two cities:" + twoCities);
 
 	}
 
